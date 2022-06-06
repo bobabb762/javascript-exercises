44cc855410eaa744d215b43839c868ce59c4768e
bo <bobabb762@gmail.com>
2022-06-06 19:16:33 -0400

    completed lessons 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,30 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let newString = ""
+    for (let i = word.length -1; i >= 0; i--) {
+    newString += word[i]
+    }
+    return newString
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    
+return array.filter(i => !args.includes(i))
+}
 
-};
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
